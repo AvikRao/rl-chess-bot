110241d93d5183adb85602a2d79dca71ce4f3fa5
AvikRao <2021arao@tjhsst.edu>
2020-08-30 20:51:51 -0400

    Create system to parse messages for commands

--- index.js
+++ index.js
@@ -15,3 +15,18 @@ client.login(auth.BOT_TOKEN);
 rl.on('line', (input) => {
 	client.channels.cache.get("749700627277545543").send(input);
 });
+
+client.on("message", (message) => {
+
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(" ");
+	const command = args.shift().toLowerCase();
+
+	if (command === "ping") {
+		const timeTaken = Date.now() - message.createdTimestamp;
+    		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+	}
+});
