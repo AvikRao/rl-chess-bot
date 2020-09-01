25a4d793f2f7f098a2386410171cffde547fd676
AvikRao <2021arao@tjhsst.edu>
2020-09-01 10:21:05 -0400

    Update fundamental functions on master branch

--- index.js
+++ index.js
@@ -6,27 +6,33 @@ const prefix = "!";
 
 const client = new Discord.Client();
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
+
+// Reset embed fields
+function embedReset () {
+    embed = new Discord.MessageEmbed()
+    .setColor("#9400D3");
+}
 
 client.login(auth.BOT_TOKEN);
 
 rl.on('line', (input) => {
-	client.channels.cache.get("749700627277545543").send(input);
+    client.channels.cache.get("704419428179378238").send(input);
 });
 
 client.on("message", (message) => {
+    embedReset();
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
 
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(" ");
+    const command = args.shift().toLowerCase();
 
-	const commandBody = message.content.slice(prefix.length);
-	const args = commandBody.split(" ");
-	const command = args.shift().toLowerCase();
-
-	if (command === "ping") {
-		const timeTaken = Date.now() - message.createdTimestamp;
-    		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-	}
+    if (command === "ping") {
+        const timeTaken = Date.now() - message.createdTimestamp;
+            message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
 });
